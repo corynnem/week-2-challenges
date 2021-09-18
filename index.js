099053d6530b9a09c886078d971f7280b8a233f1
Corynne Moody <cmoody@elevenfifty.org>
2021-09-18 15:41:48 -0400

    added 6 challenges on Javascript operators

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
 let inputs = document.querySelectorAll('input')
 
 const solved = (e, i) => {
-    console.log( 'hfkdjafkldsjal;')
     console.log(e.target.className, e.target.value)
     if(e.target.value === e.target.className) {
         inputs[i].style = 'background-color: #caffbf;'
     } else {
-        if(e.target.className === 'curly'){
-            e.target.value === '}' ? inputs[i].style = 'background-color: #caffbf;' : e.target.value === '{' ? inputs[i].style = 'background-color: #caffbf;' : inputs[i].style = 'background-color: #ffadad;'
-        } else if(e.target.className === 'paren') {
-            e.target.value === ')' ? inputs[i].style = 'background-color: #caffbf;' : e.target.value === '(' ? inputs[i].style = 'background-color: #caffbf;' : inputs[i].style = 'background-color: #ffadad;'
+        if(e.target.className === 'opencurly'){
+            e.target.value === '{' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'closecurly') {
+            e.target.value === '}' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'  
+        } else if(e.target.className === 'openparen') {
+            e.target.value === '(' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;' 
+        } else if(e.target.className === 'closeparen') {
+            e.target.value === ')' ? inputs[i].style = 'background-color: #caffbf;' : inputs[i].style = 'background-color: #ffadad;'
         } else if(e.target.className === 'questionmark') { 
             e.target.value === '?' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
         } else if(e.target.className === 'colon') {
             e.target.value === ':' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
-        } else {
+        } else if(e.target.className === 'semicolon') {
+            e.target.value === ';' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'strictE') {
+            e.target.value === '===' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'equal') {
+            e.target.value === '==' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'notEqual') {
+            e.target.value === '!==' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'greatOrE') {
+            e.target.value === '>=' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'lessOrE') {
+            e.target.value === '<=' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'and') {
+            e.target.value === '&&' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'or') {
+            e.target.value === '||' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } 
+        
+
+
+       
+        
+        else if(e.target.className === 'bonus1'){
+            e.target.value === 'num is less than 10' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'bonus2') {
+            e.target.value === 'true' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        } else if(e.target.className === 'bonus3') {
+            e.target.value === 'B' ? inputs[i].style = 'background-color: #caffbf;' :  inputs[i].style = 'background-color: #ffadad;'
+        }
+        
+        
+        else {
             inputs[i].style = 'background-color: #ffadad;'
         }
     }
 }
 
 
-console.log(inputs)
 
  for(let i=0; i < inputs.length; i++) {
     inputs[i].addEventListener('change', (e) => solved(e, i))
  }
-inputs[0]
+
+
+
